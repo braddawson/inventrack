1222e6970a930512ee319a8699add10c42a2ead6
Brad Dawson <wbd143@gmail.com>
2015-06-13 11:19:05 -0500

    corrected packaging inventory controller

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/", {templateUrl: "partials/home.html", controller: "MainCtrl"})
     .when("/batch_builder", {templateUrl: "partials/batch_builder.html", controller: "BatchBuilderCtrl"})
     .when('/cooking_forecast', {templateUrl:'partials/cooking_forecast.html', controller:'CookingForecastCtrl'})
+    .when('/cooking_inventory', {templateUrl:'partials/cooking_inventory.html', controller:'CookingInventoryCtrl'})
     .when('/kitchen_inventory', {templateUrl:'partials/kitchen_inventory.html', controller:'KitchenInventoryCtrl'})
     .when('/packaging_forecast', {templateUrl:'partials/packaging_forecast.html', controller:'PackagingForecastCtrl'})
     .when('/packaging_inventory', {templateUrl:'partials/packaging_inventory.html', controller:'PackagingInventoryCtrl'})
