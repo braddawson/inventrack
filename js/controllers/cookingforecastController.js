7550981c09bae701e51f0a2285f33099c3f1252c
Brad Dawson <wbd143@gmail.com>
2015-07-03 12:01:54 -0500

    changes to cookingforecast html and controller

--- js/controllers/cookingforecastController.js
+++ js/controllers/cookingforecastController.js
@@ -1,4 +1,10 @@
-invenApp.controller('CookingForecastCtrl', function ($scope) {
+invenApp.controller('CookingForecastCtrl', function ($scope, $firebaseObject) {
   console.log("Cooking Forecast Controller.");
   $scope.title = 'Cooking Forecast';
+
+  var ref = new Firebase("https://invenapp.firebaseio.com/cookingforecast");
+
+  // create a synchronized array
+  $scope.cookingforecast = $firebaseObject(ref);
+
 });
